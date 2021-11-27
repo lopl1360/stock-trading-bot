3627a87357801a8b944416ff75acbbe07817a451
Pezhman <lopl@lopl.net>
2021-11-26 19:32:33 -0800

    Limit number of days to get news for

--- constants.py
+++ constants.py
@@ -1,7 +1,8 @@
-STOCK_MARKET_OPEN_TIME = "06:00"
-STOCK_MARKET_CLOSE_TIME = "12:55"
+STOCK_MARKET_OPEN_TIME = "14:30"
+STOCK_MARKET_CLOSE_TIME = "20:55"
 MAX_STOP_LOSS_PERCENT = 0.07
-STOCK_SCANNER_PARTITION_COUNT = 20
+STOCK_SCANNER_PARTITION_COUNT = 40
 STOCKS_TO_CHECK = ["AAPL", "INTC", "AMD", "NVDA", "TSLA", "FB", "MSFT", "AVGO", "QCOM", "AMZN", "GOOG", "IBM", "TWTR", "NFLX", "TWLO", "ETSY", "ROKU", "ZIOP"] 
 ARTICLE_MIN_COUNT_NEWS = 10 # needs ARTICLE_MIN_COUNT_NEWS articles or more to buy stocks based on news classifier
 GET_GREAT_STOCKS = False # to get undervalues and high gainer stocks
+NEWS_DAYS_AGO = 1 # How many days to get news for
--- news.py
+++ news.py
@@ -4,20 +4,23 @@ import pprint
 import stock_data_gatherer as sdg
 import re
 import util
+import datetime
+import constants as const
 
 
 class NewsGetter:
     def __init__(self):
+        print(cred.NEWS_API_KEY)
         self.api = NewsApiClient(api_key=cred.NEWS_API_KEY)
 
     def get_news(self, stock):
         search_terms=stock + " " + re.sub(r'[^\w\s]','',sdg.get_stock_company_name(stock))
-        articles = self.api.get_everything(q=stock, qintitle=stock + " OR " + re.sub(r'[^\w\s]','',sdg.get_stock_company_name(stock)), language='en', page_size=100)['articles']
+        from_date = datetime.date.today() - datetime.timedelta(days=const.NEWS_DAYS_AGO)
+        articles = self.api.get_everything(from_param=str(from_date), q=stock, qintitle=stock + " OR " + re.sub(r'[^\w\s]','',sdg.get_stock_company_name(stock)), language='en', page_size=100)['articles']
         # qintitle=stock + " OR " + re.sub(r'[^\w\s]','',sdg.get_stock_company_name(stock))
         news_descriptions = []
+        print("Checking NEWS for ", stock)
         for news in articles:
             if util.check_overlap(search_terms, news['description']):
-                # print(search_terms)
-                # print(news['description'])
                 news_descriptions.append(news['description'])
         return news_descriptions
